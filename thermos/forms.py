e9d05a99aa3a6a7fa5de52b17d38a6e131b746dc
Ryan January <ryan.january@egov.com>
2016-07-16 20:46:22 -0500

    Finally fixed after a failed refactoring

--- thermos/forms.py
+++ thermos/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import Form
 from wtforms.fields import StringField
-from flask.ext.wtf.html5 import URLField
+from flask_wtf.html5 import URLField
 from wtforms.validators import DataRequired, url
 
 
 class BookmarkForm(Form):
-    url = URLField('url', validators=[DataRequired(), url()])
-    description = StringField('description')
+    url = URLField('The URL of your bookmark:', validators=[DataRequired(), url()])
+    description = StringField('Add an optional description:')
 
     def validate(self):
         if not self.url.data.startswith("http://") or \
--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,34 +1,40 @@
-from datetime import datetime
+import os
+
 from flask import Flask, render_template, request, redirect, url_for, flash
-from logging import DEBUG
-from forms import BookmarkForm
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.logger.setLevel(DEBUG)
-
-bookmarks=[]
 app.config['SECRET_KEY'] = '\xf7]\x88\x89\x8a\xd8\x99\x0cf\x91\xc9c\x9f\n\x03\x9f8\xadA\xd2\xfc\xf5\x17\x0e'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'thermos.db')
+app.config['DEBUG'] = True
+db = SQLAlchemy(app)
 
-def store_bookmark(url):
-    bookmarks.append(dict(
-        url = url,
-        user = "rjanuary",
-        date = datetime.utcnow()
-    ))
-class User:
-    def __init__(self,firstname,lastname):
-        self.firstname=firstname
-        self.lastname=lastname
-    def initials(self):
-        return '{}. {}.'.format(self.firstname[0],self.lastname[0])
-    def __str__(self):
-        return '{} {}'.format(self.firstname,self.lastname)
+import models
+import views
 
+from forms import BookmarkForm
+# from models import Bookmark
+import models
+
+# class User:
+#     def __init__(self,firstname,lastname):
+#         self.firstname=firstname
+#         self.lastname=lastname
+#     def initials(self):
+#         return '{}. {}.'.format(self.firstname[0],self.lastname[0])
+#     def __str__(self):
+#         return '{} {}'.format(self.firstname,self.lastname)
+#
+# fake login
+def logged_in_user():
+    return models.User.query.filter_by(username='rjanuary').first()
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='hey', text=['this','thing'], user=User('ryan','january'))
+    return render_template('index.html', new_bookmarks=models.Bookmark.newest(5))
 
 
 @app.route('/add', methods=['GET','POST'])
@@ -36,7 +42,10 @@ def add():
     form = BookmarkForm()
     if form.validate_on_submit():
         url = form.url.data
-        store_bookmark(url)
+        description = form.description.data
+        bm = models.Bookmark(user=logged_in_user(),url=url, description=description)
+        db.session.add(bm)
+        db.session.commit()
         # app.logger.debug("Stored Bookmark: '{}'".format(url))
         flash("Stored Bookmark: '{}'".format(url))
         return redirect(url_for('index'))
@@ -52,5 +61,5 @@ def server_error(e):
     return render_template("500.html"),500
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
