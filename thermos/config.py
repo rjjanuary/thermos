84f06963f8fd4fe0c512be773de97ead010f10f9
Ryan January <ryan.january@egov.com>
2016-08-05 13:24:29 -0500

    modified statsd to point to local host

--- thermos/config.py
+++ thermos/config.py
@@ -6,7 +6,7 @@ class Config:
     SECRET_KEY = '\xf7]\x88\x89\x8a\xd8\x99\x0cf\x91\xc9c\x9f\n\x03\x9f8\xadA\xd2\xfc\xf5\x17\x0e'
     DEBUG = False
     SQLALCHEMY_RECORD_QUERIES = False
-    STATSD_HOST = '10.162.2.45'
+    STATSD_HOST = '127.0.0.1'
     STATSD_PORT = 8125
 
 class DevelopmentConfig(Config):
