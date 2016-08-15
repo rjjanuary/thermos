8d5d8df42be230d696d2876898ebacbf6269ab77
Ryan January <ryan.january@egov.com>
2016-08-15 11:18:33 -0500

    Added webrequests service files

---
+++ external_config/webrequests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+web_host=192.168.2.5;
+while true; do
+  curl http://$web_host/bookmarks/user/user1 > /dev/null;
+  curl http://$web_host/bookmarks/user/user2 > /dev/null;
+  curl http://$web_host/bookmarks/user/user3 > /dev/null;
+  curl http://$web_host/bookmarks/user/user4 > /dev/null;
+  curl http://$web_host/bookmarks/user/user99999 > /dev/null;
+done
