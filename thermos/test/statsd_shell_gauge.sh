c82b8994c85183814e9abc21cbb4d558712736aa
Ryan January <ryan.january@egov.com>
2016-08-23 01:45:17 -0500

    added shell scripts

--- thermos/test/statsd_shell_gauge.sh
+++ thermos/test/statsd_shell_gauge.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "sending single packet to statsd"
-echo "example,city=austin,state=tx:114|g" | nc -uw 1 localhost 8125
+echo "example_gauge,city=austin,state=tx:114|g" | nc -uw 1 localhost 8125
 
